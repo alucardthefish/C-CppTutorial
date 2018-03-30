ee28046e42e1687596af87420d43a76df0c0dde1
jaerjaerbin <jaerma74@yahoo.com>
2018-03-30 18:53:00 +0000

    Implementing some functions like factorial and some others for printing

--- 9-tutorial/memoryaddress.c
+++ 9-tutorial/memoryaddress.c
@@ -2,6 +2,20 @@
 
 #include <stdio.h>
 
+//Función que imprime en consola atributos de un contacto
+void address()
+{
+    printf("\nName of the Person");
+    printf("\nStreet, Apartment//House No. ");
+    printf("\nzip, City");
+    printf("\nCountry");
+}
+
+// Declaraciones
+void margenes(char flag[], char title[]);
+int abs(int n);
+int factorial(int x);
+
 int main()
 {
     int a = 10;
@@ -18,7 +32,54 @@ int main()
     printf("La dirección que almacena ptr: %p\n", ptr);
     printf("La dirección de vari1: %p\n", &vari1);
     
+    address();
+    address();
+    
+    margenes("header", "Valor absoluto");
+    printf("El valor absoluto del numero: -43 es: \n");
+    int res = abs(-43);
+    printf("abs(-43) = %d\n", res);
+    margenes("fin", "Valor absoluto");
+    
+    margenes("header", "factorial");
+    int z, fact;
+    printf("\nIngresa cualquier numero entero "); scanf("%d", &z);
+    fact = factorial(z);
+    printf("\nEl Factorial de %d es = %d", z, fact);
+    margenes("fin", "factorial");
+    
     return 0;
+}
+
+// Funcion para poner un encabezado y pie pagina en consola
+void margenes(char flag[], char title[])
+{
+    if(flag == "header"){
+        printf("\n-------------------------Inicio-prueba:-%s-------------------------\n\n",title);
+    }
+    else{
+        printf("\n-------------------------Final--prueba:-%s-------------------------\n\n",title);
+    }
+}
+
+// Funcion que devuelve el valor absoluto de un numero
+int abs(int n)
+{
+    if(n < 0){
+        n = n * -1;
+    }
+    return n;
+}
+
+// Funcion que calcula el numero de factorial de un numero x dado
+int factorial(int x)
+{
+    int f = 1, i;
+    for(i = x; i >=1; i--)
+    {
+        f = f*i;
+    }
+    return f;
 }
 
 
